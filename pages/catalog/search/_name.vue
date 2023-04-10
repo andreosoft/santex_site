29a66b128d7a8a307fe945d42898f09915a45c94
kenny <pletnev735@gmail.com>
2023-04-10 11:24:28 +0300

    commit Kirill

--- pages/catalog/search/_name.vue
+++ pages/catalog/search/_name.vue
@@ -3,26 +3,11 @@
     <v-divider class="mb-8" />
     <common-beadcrumbs class="mb-4" :value="breadcrumbsData" />
     <h1>Поиск</h1>
-    <!-- <p>name {{valueFilters}}</p> -->
     <div class="mb-10">
         <p><b>Вы искали: </b><span class="underlined">{{searchInput}}</span>, найдено {{ data.length }} шт.</p>
     </div>
     <v-divider class="mb-10"/>
    <base-catalog :value="searchInput" :data="data" :dataFilters="dataFilters" :valueFilters="valueFilters" :pager="pager" :sort="sort" @update-data="updateValueFilters"/>
-   <!-- <test :value="valueFilters"/> -->
-<!--    <v-row  class="s-row">-->
-<!--      <v-col cols="3">-->
-<!--        <catalog-filter v-model="filter" />-->
-<!--      </v-col>-->
-<!--      <v-col cols="9">-->
-<!--        <catalog-top-bar />-->
-<!--        <v-row  class="s-row">-->
-<!--          <v-col cols="4" v-for="(el, i) in data" :key="i">-->
-<!--            <catalog-item-list :el="el" />-->
-<!--          </v-col>-->
-<!--        </v-row>-->
-<!--      </v-col>-->
-<!--    </v-row>-->
     <div class="text-center mt-10 ">
       <v-pagination v-model="page" :length="4" circle></v-pagination>
     </div>
@@ -32,7 +17,7 @@
 <script>
 import {getData} from "@/pages/catalog/getData";
 import BaseCatalog from "@/components/catalog/base-catalog.vue";
-// console.log(searchInput);
+
 export default {
   components: {BaseCatalog},
   data() {
@@ -68,13 +53,11 @@ export default {
         let p = await getData({ route: this.$route, $axios: this.$axios, $config: this.$config });
         this.data = p.data;
         this.pager = p.pager;
+        this.searchInput = p.searchInput;
       },
     },
     },
     async asyncData({ route, $axios, $config }) {
-      // console.log(route);
-      // console.log($axios);
-      // console.log($config);
       return await getData({ route, $axios, $config });
     },
 }
