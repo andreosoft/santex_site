28b1fd006a8b47b3d487f7d21708d2b311768549
Andrei <andreyzagorets@gmail.com>
2023-07-11 17:28:03 +0300

    commit

--- pages/catalog/getData.js
+++ pages/catalog/getData.js
@@ -10,6 +10,7 @@ export async function getData({ route, $axios, $config }) {
   Object.assign(filters, { status: 1 });
   if (category_id) Object.assign(filters, { category_id: category_id });
   if (searchInput) Object.assign(filters, { "OR": [
+    {id: { condition: "LIKE", value: "%" + searchInput + "%" }},
     {name: { condition: "LIKE", value: "%" + searchInput + "%" }},
     {vendor: { condition: "LIKE", value: "%" + searchInput + "%" }},
     {factory_article: { condition: "LIKE", value: "%" + searchInput + "%" }}] });
