c9c203cf5aa0f53d8c20727fe429f66a20ebf3d6
Andrei <andreyzagorets@gmail.com>
2023-08-23 10:59:36 +0300

    commit

--- components/s/guideStyleItemsEl.vue
+++ components/s/guideStyleItemsEl.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <NuxtLink :to="'/interior/' + el.id">
-      <img :src="$config.baseImageURL + el.images" style="width: 100%;"/>
+      <img :src="$config.baseImageURL + el.images[0]" style="width: 100%;"/>
       <div style="position: absolute; bottom: 50px; left: 0; right: 0">
         <div class="pa-2 text-center">
           <v-btn class="s-btn-text">Перейти в интерьер</v-btn>
