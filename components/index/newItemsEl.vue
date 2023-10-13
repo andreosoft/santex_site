294c53989575b1f6fb1d85ef21dbd2ea657056b1
kenny <pletnev735@gmail.com>
2023-10-13 11:25:54 +0300

    update collection_Kirill

--- components/index/newItemsEl.vue
+++ components/index/newItemsEl.vue
@@ -1,17 +1,17 @@
 <template>
   <v-card>
     <NuxtLink :to="'/catalog/' + el.id">
-      <v-img :src="$config.baseImageURL + el.images" />
+      <v-img :src="el.img" />
       <div class="s-cart-tite-1">
         <div class="pa-10">
           <div class="d-inline-block mr-3" style="vertical-align: top">
             <v-btn fab><img src="/icons/basket.svg" alt="" /></v-btn>
           </div>
-          <div class="d-inline-block dark--text">
+          <div class="d-inline-block white--text">
             <div class="text-uppercase text-h5">
-              {{ el.name }}
+              <b>{{ el.titleCollection }}</b>
             </div>
-            <!-- <div>{{ el.titleItem }} <i class="fa fa-long-arrow-right"></i></div> -->
+            <div><b>{{ el.titleItem }}</b> <i class="fa fa-long-arrow-right"></i></div>
           </div>
         </div>
       </div>
