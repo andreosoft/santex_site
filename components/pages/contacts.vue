a0405e53e2b518fa52837e96f2b9846d6f066e52
kenny <pletnev735@gmail.com>
2024-03-28 17:04:34 +0300

    updates_Kirill

--- components/pages/contacts.vue
+++ components/pages/contacts.vue
@@ -4,50 +4,70 @@
         <v-row>
             <v-col cols="8">
                 <p class="grey--text"><b>АДРЕС</b></p>
-                <p style="font-size: 22px; ">г. Москва, Волгоградский проспект, д. 32, корпус 25,<br/>
-                    ТЦ «Метр квадратный», 2 этаж, пав. № 66-67-96</p>
+                <p style="font-size: 22px">
+                    г. Москва, Волгоградский проспект, д. 32, корпус 25,<br />
+                    ТЦ «Метр квадратный», 2 этаж, пав. № 66-67-96
+                </p>
             </v-col>
             <v-col cols="4">
                 <p class="grey--text">
                     <b>Режим работы</b>
                 </p>
-                <p style="font-size: 22px;">
-                    Ежедневно с 10:00 до 20:00
-                </p>
+                <p style="font-size: 22px">Ежедневно с 10:00 до 20:00</p>
             </v-col>
         </v-row>
         <v-row>
             <v-col cols="8">
-                <p class="grey--text"><b>ТЕЛЕФОНЫ</b></p>
                 <v-row>
                     <v-col cols="6">
-                        <h4 class="pb-2"><a href="tel://88007759985">8 (800) 775-99-85</a></h4>
+                        <h4 class="pb-2">
+                            <p class="grey--text"><b>ТЕЛЕФОНЫ</b></p>
+                            <a href="tel://88007759985">8 (800) 775-99-85</a>
+                        </h4>
                         <p>Звонок по России бесплатный</p>
                     </v-col>
                     <v-col cols="6">
-                        <h4 class="pb-2"><a href="tel://88007759985">8 (495) 225 62 85</a></h4>
+                        <p class="grey--text" style="font-size: 20px">
+                            <b>САНТЕХНИКА</b>
+                        </p>
+                        <h4 class="pb-2">
+                            <a href="tel://79672823107">+7 (967) 282-31-07</a>
+                        </h4>
                         <p>Ежедневно с 10:00 до 20:00</p>
                     </v-col>
                 </v-row>
             </v-col>
             <v-col cols="4">
-                <p class="grey--text">
-                    <b>E-mail</b>
+                <p class="grey--text" style="font-size: 20px">
+                    <b>ПЛИТКА</b>
                 </p>
-                <h4><a href="mailto:info@santax.ru">info@santax.ru</a></h4>
+                <h4 class="pb-2">
+                    <a href="tel://79055368559">+7 (905) 536-85-59</a>
+                </h4>
+                <p>Ежедневно с 10:00 до 20:00</p>
             </v-col>
         </v-row>
         <v-row class="mb-4">
             <v-col cols="4">
-                <a href="https://www.instagram.com/santehkomfort?igsh=MTN6OWVmaTNlcHMwcQ==" class="d-flex align-center mb-6 ">
-                    <div class="pa-1 pr-4"><img src="/icons/contact/inst.png" /></div>
+                <a
+                    href="https://www.instagram.com/santehkomfort?igsh=MTN6OWVmaTNlcHMwcQ=="
+                    class="d-flex align-center mb-6"
+                >
+                    <div class="pa-1 pr-4">
+                        <img src="/icons/contact/inst.png" />
+                    </div>
                     <div>
                         <div class="grey--text"><b>Instagram</b></div>
                         <div><b>santehkomfort</b></div>
                     </div>
                 </a>
-                <a href="https://instagram.com/tanya_prodesign" class="d-flex align-center">
-                    <div class="pa-1 pr-4"><img src="/icons/contact/inst.png" /></div>
+                <a
+                    href="https://instagram.com/tanya_prodesign"
+                    class="d-flex align-center"
+                >
+                    <div class="pa-1 pr-4">
+                        <img src="/icons/contact/inst.png" />
+                    </div>
                     <div>
                         <div class="grey--text"><b>Instagram</b></div>
                         <div><b>tanya_prodesign</b></div>
@@ -55,15 +75,25 @@
                 </a>
             </v-col>
             <v-col cols="4">
-                <a href="https://api.whatsapp.com/send/?phone=79672823107" class="d-flex align-center mb-6 ">
-                    <div class="pa-1 pr-4"><img src="/icons/contact/whatsapp.png" /></div>
+                <a
+                    href="https://api.whatsapp.com/send/?phone=79672823107"
+                    class="d-flex align-center mb-6"
+                >
+                    <div class="pa-1 pr-4">
+                        <img src="/icons/contact/whatsapp.png" />
+                    </div>
                     <div>
                         <div class="grey--text"><b>Сантехника</b></div>
                         <div><b>+7 (967) 282-31-07</b></div>
                     </div>
                 </a>
-                <a href="https://api.whatsapp.com/send/?phone=79055368559" class="d-flex align-center">
-                    <div class="pa-1 pr-4"><img src="/icons/contact/whatsapp.png" /></div>
+                <a
+                    href="https://api.whatsapp.com/send/?phone=79055368559"
+                    class="d-flex align-center"
+                >
+                    <div class="pa-1 pr-4">
+                        <img src="/icons/contact/whatsapp.png" />
+                    </div>
                     <div>
                         <div class="grey--text"><b>Плитка</b></div>
                         <div><b>+7 (905) 536-85-59</b></div>
@@ -71,8 +101,17 @@
                 </a>
             </v-col>
             <v-col cols="4">
-                <a href="https://youtube.com/@tanyaprodesign" class="d-flex align-center mb-6 ">
-                    <div class="pa-1 pr-4"><img src="/icons/contact/youtube.png" /></div>
+                <p class="grey--text">
+                    <b>E-mail</b>
+                </p>
+                <h4><a href="mailto:info@santax.ru">info@santax.ru</a></h4>
+                <a
+                    href="https://youtube.com/@tanyaprodesign"
+                    class="d-flex align-center mb-6"
+                >
+                    <div class="pa-1 pr-4">
+                        <img src="/icons/contact/youtube.png" />
+                    </div>
                     <div>
                         <div class="grey--text"><b>Youtube</b></div>
                         <div><b>Tanya PROdesign</b></div>
@@ -90,29 +129,39 @@
             <div>
                 <div class="d-flex mb-4">
                     <div class="mr-6"><i class="fas fa-check-circle"></i></div>
-                    <div>В навигаторе «МЕТР КВАДРАТНЫЙ» ПАРКОВКА НА ТЕРРИТОРИИ БЕСПЛАТНАЯ.</div>
-                </div>
-                <div class="d-flex mb-4">
-                    <div class="mr-6"><i class="fas fa-check-circle"></i></div>
-                    <div>От метро «Волгоградский проспект», первый вагон из центра, после турникетов налево, идти прямо
-                        700 метров.
-                        Проходите СБЕРБАНК И ЗА НИМ «ТЦ Метр квадратный». </div>
-                </div>
-                <div class="d-flex mb-4">
-                    <div class="mr-6"><i class="fas fa-check-circle"></i></div>
-                    <div>От МЦК «Угрешская» , после турникетов направо в сторону Волгоградского проспекта пройти 400
-                        метров до «ТЦ Метр квадратный».
+                    <div>
+                        В навигаторе «МЕТР КВАДРАТНЫЙ» ПАРКОВКА НА ТЕРРИТОРИИ
+                        БЕСПЛАТНАЯ.
                     </div>
                 </div>
                 <div class="d-flex mb-4">
                     <div class="mr-6"><i class="fas fa-check-circle"></i></div>
-                    <div>Как нас найти в ТЦ «Метр Квадратный»: Центральный вход. В УЗКОМ ПРОЕЗДЕ. (в офисный центр НЕ
-                        НАДО) Заходите в СРЕДНЮЮ
-                        ДВЕРЬ вход с КРАСНЫМ козырьком, поднимаетесь на 2-й этаж (самый верхний) на лифте или на
-                        эскалаторе. С эскалатора направо -
-                        линия Ж, наклейки с номерами павильонов на полу Из лифта прямо - линия Ж, наклейки с номерами
-                        павильонов на полу. За домом
-                        обоев, наш самый большой павильон с сантехникой и плиткой.</div>
+                    <div>
+                        От метро «Волгоградский проспект», первый вагон из
+                        центра, после турникетов налево, идти прямо 700 метров.
+                        Проходите СБЕРБАНК И ЗА НИМ «ТЦ Метр квадратный».
+                    </div>
+                </div>
+                <div class="d-flex mb-4">
+                    <div class="mr-6"><i class="fas fa-check-circle"></i></div>
+                    <div>
+                        От МЦК «Угрешская» , после турникетов направо в сторону
+                        Волгоградского проспекта пройти 400 метров до «ТЦ Метр
+                        квадратный».
+                    </div>
+                </div>
+                <div class="d-flex mb-4">
+                    <div class="mr-6"><i class="fas fa-check-circle"></i></div>
+                    <div>
+                        Как нас найти в ТЦ «Метр Квадратный»: Центральный вход.
+                        В УЗКОМ ПРОЕЗДЕ. (в офисный центр НЕ НАДО) Заходите в
+                        СРЕДНЮЮ ДВЕРЬ вход с КРАСНЫМ козырьком, поднимаетесь на
+                        2-й этаж (самый верхний) на лифте или на эскалаторе. С
+                        эскалатора направо - линия Ж, наклейки с номерами
+                        павильонов на полу Из лифта прямо - линия Ж, наклейки с
+                        номерами павильонов на полу. За домом обоев, наш самый
+                        большой павильон с сантехникой и плиткой.
+                    </div>
                 </div>
             </div>
         </div>
@@ -143,13 +192,13 @@
 
 <script>
 export default {
-data() {
-    return {
-        showPopupConsult: false,
-        videoItem: {
-                video: "https://www.youtube.com/embed/nOKam63GgzQ"
+    data() {
+        return {
+            showPopupConsult: false,
+            videoItem: {
+                video: "https://www.youtube.com/embed/nOKam63GgzQ",
             },
-        }
+        };
     },
-}
+};
 </script>
