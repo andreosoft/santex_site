83403951adb1b6551a85ae40abaf7c3c8f2e3127
Andrei <andreyzagorets@gmail.com>
2024-04-04 15:09:13 +0300

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { charset: 'utf-8' },
       // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { id: 'description', name: 'description', content: 'Сантехкомфорт Элит магазин сантехнических материалов' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
