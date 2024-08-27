780099a6037db2efcaf08181815c06f87ab1b7e6
Andrei <andreyzagorets@gmail.com>
2024-08-27 09:36:25 +0300

    commit

--- components/catalog/filter.vue
+++ components/catalog/filter.vue
@@ -1,90 +1,47 @@
 <template>
   <div class="parent">
-    <catalog-filterResult 
-      ref="filterResult"
-      @filterResult="onUpdateData" 
-      :locationRes="dy" 
-      :resultData="resultData.count"
-    />
+    <catalog-filterResult ref="filterResult" @filterResult="onUpdateData" :locationRes="dy" :resultData="resultData" />
     <div class="space-check">
-      <catalog-price 
-      @location="locationResult" 
-      title="Цена, руб." 
-      v-model="dataPrice" 
-      :max="activeFilters && activeFilters?.price ? activeFilters?.price?.max_price : filters?.price?.max_price" 
-      :min="activeFilters && activeFilters?.price ? activeFilters?.price?.min_price : filters?.price?.min_price"
-      />
-        <catalog-brands
-        v-if="filters.brands && filters.brands.length>1"
-        @location="locationResult"
-        :params="filters.brands"
-        v-model="dataF.brand"
-        :activeParams="resultData && resultData?.brands ? resultData?.brands : activeFilters?.brands"
-        :dataF="dataF"
-        />
-        <v-divider v-if="filters.brands && filters.brands.length>1" class="my-4" />
-        <catalog-collections
-        class="mt-4"
-        v-if="filters.collections && filters.collections.length>1"
-        @location="locationResult"
-        :params="filters.collections"
-        :activeParams="resultData && resultData?.collections ? resultData?.collections : activeFilters?.collections"
-        v-model="dataF.collection"
-        :dataF="dataF"
-        />
-        <v-divider v-if="filters.collections && filters.collections.length>1" class="my-4" />
-        <catalog-categories class="mt-4"
-        v-if="filters.categories && filters.categories.length>1"
-        @location="locationResult"
-        :params="filters.categories"
-        :activeParams="resultData && resultData?.categories ? resultData?.categories : activeFilters?.categories"
-        v-model="dataF.category_id"
-        :dataF="dataF"
-        />
-      <v-divider  v-if="filters.categories && filters.categories.length>1" class="my-4" />
+      <catalog-price @location="locationResult" title="Цена, руб." v-model="dataPrice"
+        :max="activeFilters && activeFilters?.price ? activeFilters?.price?.max_price : filters?.price?.max_price"
+        :min="activeFilters && activeFilters?.price ? activeFilters?.price?.min_price : filters?.price?.min_price" />
+
+      <catalog-brands v-if="filters.brands && filters.brands.length > 1" @location="locationResult"
+        :params="filters.brands" v-model="dataF.brand" :activeParams="activeFilters.brands" :dataF="dataF" />
+      <v-divider v-if="filters.brands && filters.brands.length > 1" class="my-4" />
+      <catalog-collections class="mt-4" v-if="filters.collections && filters.collections.length > 1"
+        @location="locationResult" :params="filters.collections" :activeParams="activeFilters.collections"
+        v-model="dataF.collection" :dataF="dataF" />
+      <v-divider v-if="filters.collections && filters.collections.length > 1" class="my-4" />
+      <catalog-categories class="mt-4" v-if="filters.categories && filters.categories.length > 1"
+        @location="locationResult" :params="filters.categories" :activeParams="activeFilters.categories"
+        v-model="dataF.category_id" :dataF="dataF" />
+      <v-divider v-if="filters.categories && filters.categories.length > 1" class="my-4" />
     </div>
     <div v-for="(el, i) in filters.filters" :key="i">
       <div>
         <div v-if="el.type == 2" class="space-check">
-          <catalog-ranges 
-          v-if="el.numFilters.length > 1" 
-          @location="locationResult" 
-          :title="el.name" 
-          v-model="dataF[el.filters_id]" 
-          :params="el.numFilters" 
-          :activeParams="activeFilters?.filters"
-          :minV="el.min" 
-          :maxV="el.max" 
-          />
+          <catalog-ranges v-if="el.numFilters.length > 1" @location="locationResult" :title="el.name"
+            v-model="dataF[el.filters_id]" :params="el.numFilters" :activeParams="activeFilters.filters" :minV="el.min"
+            :maxV="el.max" />
         </div>
         <div v-else class="space-check">
-          <catalog-check1 
-          v-if="el.filters.length > 1" 
-          v-model="dataF[el.filters_id]" 
-          @location="locationResult" 
-          :idFilters="el.filters_id"
-          :title="el.name" 
-          :params="el.filters" 
-          :activeParams="resultData && resultData?.filters ? resultData?.filters : activeFilters?.filters"
-          :filters_data="el.filters_data" 
-          :dataF="dataF"
-          />
+          <catalog-check1 v-if="el.filters.length > 1" v-model="dataF[el.filters_id]" @location="locationResult"
+            :idFilters="el.filters_id" :title="el.name" :params="el.filters" :activeParams="activeFilters.filters"
+            :filters_data="el.filters_data" :dataF="dataF" />
           <v-divider v-if="el.filters.length > 1" class="my-4" />
         </div>
       </div>
     </div>
-    <v-btn 
-      @click="toggleFunction($event)" 
-      outlined
-      class="s-btn-cart s-btn-text clearBtn w-100 mt-3">       
+    <v-btn @click="toggleFunction($event)" outlined class="s-btn-cart s-btn-text clearBtn w-100 mt-3">
       Сбросить фильтр
-       <img class="ml-2 el_card" src="/icons/Close catalog.svg" alt="">
-     </v-btn>
+      <img class="ml-2 el_card" src="/icons/Close catalog.svg" alt="">
+    </v-btn>
   </div>
 </template>
 
 <script>
-import {debounce} from "lodash";
+import { debounce } from "lodash";
 export default {
   props: {
     value: Object,
@@ -122,18 +79,18 @@ export default {
   //     //     }
   //   })
   // },
-  watch:{
-    activeFilters: function(){
-      if(this.activeFilters) {
+  watch: {
+    activeFilters: function () {
+      if (this.activeFilters) {
         this.timeFilter = 1000
       }
     },
-    filters: function(){this.initValueFilters();},
-    value: function(){
-        this.initValueFilters();
-        this.value.brand ? this.dataF.brand = this.value.brand : this.dataF.brand = [];
-        this.value.collection ? this.dataF.collection = this.value.collection : this.dataF.collection = [];
-        this.value.category_id ? this.dataF.category_id = this.value.category_id : this.dataF.category_id = [];
+    filters: function () { this.initValueFilters(); },
+    value: function () {
+      this.initValueFilters();
+      this.value.brand ? this.dataF.brand = this.value.brand : this.dataF.brand = [];
+      this.value.collection ? this.dataF.collection = this.value.collection : this.dataF.collection = [];
+      this.value.category_id ? this.dataF.category_id = this.value.category_id : this.dataF.category_id = [];
     }
   },
   // computed: {
@@ -149,11 +106,11 @@ export default {
   //   }
   // },
   methods: {
-    toggleFunction(e){
+    toggleFunction(e) {
       this.clearFilters();
       this.locationResult(e.target);
     },
-    clearFilters(){
+    clearFilters() {
       this.dataF = {
         brand: [],
         category_id: [],
@@ -179,19 +136,19 @@ export default {
       this.dataPrice = this.value.price ? this.value.price : [];
       for (const key in this.filters.filters) {
         let f = [];
-        if (this.value?.f[this.filters.filters[key]["filters_id"]]){
+        if (this.value?.f[this.filters.filters[key]["filters_id"]]) {
           f = this.value.f[this.filters.filters[key]["filters_id"]];
-        } 
+        }
         // console.log(this.filters.filters[key]["filters_id"], f);
         this.$set(this.dataF, this.filters.filters[key]["filters_id"], f);
       }
-        // for (const key in this.filters.brands) {
-        //   if(this.filters.brands[key].brand){
-        //     let f = [];
-        //     if (this.value?.f[this.filters.brands[key].brand]) f = this.value.f[this.filters.brands[key].brand];
-        //     this.$set(this.dataF["brands"], this.filters.brands[key].brand, f);
-        //   }
-        // }
+      // for (const key in this.filters.brands) {
+      //   if(this.filters.brands[key].brand){
+      //     let f = [];
+      //     if (this.value?.f[this.filters.brands[key].brand]) f = this.value.f[this.filters.brands[key].brand];
+      //     this.$set(this.dataF["brands"], this.filters.brands[key].brand, f);
+      //   }
+      // }
     },
     onUpdateData() {
       let r = {};
@@ -203,10 +160,10 @@ export default {
 
 
       // Подгрузка товаров
-      this.$emit('input', { 
+      this.$emit('input', {
         f: r,
-        price: this.dataPrice, 
-        brand: this.dataF.brand.length !== 0 ? this.dataF.brand : [], 
+        price: this.dataPrice,
+        brand: this.dataF.brand.length !== 0 ? this.dataF.brand : [],
         category_id: this.dataF.category_id.length !== 0 ? this.dataF.category_id : [],
         collection: this.dataF.collection.length !== 0 ? this.dataF.collection : []
       });
@@ -214,7 +171,7 @@ export default {
       this.dy = 0;
       window.scrollTo(0, 0);
     },
-    locationResult: debounce(async function(v){
+    locationResult: debounce(async function (v) {
       try {
         let rect = v.getBoundingClientRect();
         let scrolltop = window.scrollY + rect.top;
@@ -241,25 +198,27 @@ export default {
           price: this.dataPrice.length !== 0 ? this.dataPrice : {}
         }
 
-        if (this.$route.query.q) Object.assign(filtersCount, { "OR": [
-            {id: { condition: "LIKE", value: "%" + this.$route.query.q + "%" }},
-            {name: { condition: "LIKE", value: "%" + this.$route.query.q + "%" }},
-            {vendor: { condition: "LIKE", value: "%" + this.$route.query.q + "%" }},
-            {factory_article: { condition: "LIKE", value: "%" + this.$route.query.q + "%" }}] });
-
-            let res, resPromote;
-            if(this.$route.name.match('catalog')){
-              res = await this.$axios.get(this.$config.baseURL + '/api/site/catalog/filters', { 
-          params: {
-            f: r, 
-            filters: filtersCount,
-          }
+        if (this.$route.query.q) Object.assign(filtersCount, {
+          "OR": [
+            { id: { condition: "LIKE", value: "%" + this.$route.query.q + "%" } },
+            { name: { condition: "LIKE", value: "%" + this.$route.query.q + "%" } },
+            { vendor: { condition: "LIKE", value: "%" + this.$route.query.q + "%" } },
+            { factory_article: { condition: "LIKE", value: "%" + this.$route.query.q + "%" } }]
         });
-            }
-        if(this.$route.name.match('promote')){
-          resPromote = await this.$axios.get(this.$config.baseURL + '/api/site/promote_catalog/filters', { 
+
+        let res, resPromote;
+        if (this.$route.name.match('catalog')) {
+          res = await this.$axios.get(this.$config.baseURL + '/api/site/catalog/filters', {
             params: {
-              f: r, 
+              f: r,
+              filters: filtersCount,
+            }
+          });
+        }
+        if (this.$route.name.match('promote')) {
+          resPromote = await this.$axios.get(this.$config.baseURL + '/api/site/promote_catalog/filters', {
+            params: {
+              f: r,
               filters: {
                 "ic.promote_id": this.$route.params.id,
                 brand: this.dataF.brand.length !== 0 ? this.dataF.brand : {},
@@ -281,7 +240,7 @@ export default {
 </script>
 
 <style lang="scss">
-.s-clear-filter{
+.s-clear-filter {
   background-color: #e0e0e0 !important;
   border: unset !important;
 }
